e017934ea8220e88aa536cc1153fbc85317297e6
Ann <ann.mitrakhovich@gmail.com>
2023-09-13 14:27:22 +0300

    task 24 done

--- hometask24.py
+++ hometask24.py
@@ -10,3 +10,22 @@
 собирает ягоды с этого куста и с двух соседних с ним.
 Напишите программу для нахождения максимального числа ягод, которое может собрать
 за один заход собирающий модуль, находясь перед некоторым кустом заданной во входном файле грядки."""
+
+lst_havest = [1, 30, 5, 0, 5, 3, 6, 10] #входной файл грядки
+""" 
+max_berries = 0
+for i in range(1, len(lst_havest) - 1):
+    berries = lst_havest[i-1] + lst_havest[i] + lst_havest[i+1]
+    if berries > max_berries:
+        max_berries = berries
+if (lst_havest[len(lst_havest) - 2] + lst_havest[len(lst_havest) - 1] + lst_havest[0]) > max_berries:
+    max_berries = (lst_havest[len(lst_havest) - 2] + lst_havest[len(lst_havest) - 1] + lst_havest[0])
+if (lst_havest[len(lst_havest) - 1] + lst_havest[0] + lst_havest[1]) > max_berries:
+    max_berries = (lst_havest[len(lst_havest) - 1] + lst_havest[0] + lst_havest[1])
+"""
+sum_berries = ([sum(lst_havest[i:i+3]) for i in range(len(lst_havest)-2)] +
+               [lst_havest[len(lst_havest)-2] + lst_havest[len(lst_havest)-1] + lst_havest[0]] +
+               [lst_havest[len(lst_havest)-1] + lst_havest[0] + lst_havest[1]])
+max_berries = max(sum_berries)
+print(max_berries)
+
