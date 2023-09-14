ea6de5058921f930ac9013fa10d78cabf8061e70
Ann <ann.mitrakhovich@gmail.com>
2023-09-14 11:36:01 +0300

    changed file

--- california_task_42.py
+++ california_task_42.py
@@ -1,4 +1,4 @@
 # Задача 42: Узнать какая максимальная households в зоне минимального значения population.
 import pandas as pd
-data = pd.read_csv('california_housing_test.csv')
+data = pd.read_csv('california_housing_train')
 print(data[data["population"] == data["population"].min()]['households'].max())
