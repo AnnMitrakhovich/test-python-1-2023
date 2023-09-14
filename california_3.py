d30b8c9371498e62a48c939b9c122618d2c5c45c
Ann <ann.mitrakhovich@gmail.com>
2023-09-14 11:19:02 +0300

    task 3 done

--- california_3.py
+++ california_3.py
@@ -8,4 +8,8 @@ median_income = 3.1250
 """
 import pandas as pd
 data = pd.read_csv('california_housing_test.csv')
-print(f'{data["median_house_value"].min()} - минимальное, {data["median_house_value"].max()} - максимальное значение median_house_value')
+#print(f'{data["median_house_value"].min()} - минимальное, {data["median_house_value"].max()} - максимальное значение median_house_value')
+
+#print(data[data['median_income'] == 3.1250]["median_house_value"].max())
+
+print(data[data["median_house_value"] == data["median_house_value"].min()]['population'].max())
