44a266f57581cedc32b5e702e6e3780e4dfb57e9
Ann <ann.mitrakhovich@gmail.com>
2023-09-14 11:00:11 +0300

    start task 3

---
+++ california_3.py
@@ -0,0 +1,10 @@
+"""
+1. Определить какое максимальное и минимальное
+значение median_house_value
+2. Показать максимальное median_house_value, где
+median_income = 3.1250
+3. Узнать какая максимальная population в зоне
+минимального значения median_house_value
+"""
+import pandas as pd
+data = pd.read_csv('california_housing_test.csv')
