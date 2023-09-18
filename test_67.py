a8f885d615403b879c9100dc741d5e699df4a83d
Ann <ann.mitrakhovich@gmail.com>
2023-09-18 11:38:55 +0300

    done

--- test_67.py
+++ test_67.py
@@ -9,3 +9,19 @@ low - маленький(до 35)
 penguins = sns.load_dataset("penguins")
 penguins.head()
 """
+import seaborn as sns
+from random import randint
+penguins = sns.load_dataset("penguins")
+
+def func(row):
+    res = randint(1, 60)
+    val = None
+    if res < 35:
+        val = "low"
+    elif 35 < res < 43:
+        val = "middle"
+    elif res > 42:
+        val = "high"
+    return val
+penguins["len"] = penguins.apply(func, axis=1)
+print(penguins)
