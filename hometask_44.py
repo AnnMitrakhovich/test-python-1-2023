d2c977fdd2ccfe60803249c3b34cb508ea6508b1
Ann <ann.mitrakhovich@gmail.com>
2023-09-19 17:21:55 +0300

    done without get_dummies

--- hometask_44.py
+++ hometask_44.py
@@ -5,11 +5,28 @@ import random
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
-data = pd.DataFrame({'whoAmI':lst})
-data.head()
+data = pd.DataFrame({'whoAmI': lst})
 print(data)
 
+
 final_data = pd.get_dummies(data["whoAmI"]).astype(int)
+print("one hot вид")
 print(final_data)
 
-#def hot_encoding_without_get_dummies():
+def rule_human(row):
+    if row == 'human':
+        return 1
+    else:
+        return 0
+def rule_robot(row):
+    if row == 'robot':
+        return 1
+    else:
+        return 0
+row_data = list(data['whoAmI'])
+convert_rows_human = list(rule_human(el) for el in row_data)
+convert_rows_robot = list(rule_robot(el) for el in row_data)
+
+res_data = pd.DataFrame({"human": convert_rows_human, "robot": convert_rows_robot})
+print("one hot вид")
+print(res_data)
