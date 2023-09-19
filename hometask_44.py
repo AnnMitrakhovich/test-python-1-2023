c3128f1d9e6d115d4cbc70a73444e5421ef178af
Ann <ann.mitrakhovich@gmail.com>
2023-09-19 17:46:16 +0300

    final result

--- hometask_44.py
+++ hometask_44.py
@@ -8,24 +8,23 @@ random.shuffle(lst)
 data = pd.DataFrame({'whoAmI': lst})
 print(data)
 
-
 final_data = pd.get_dummies(data["whoAmI"]).astype(int)
 print("one hot вид")
 print(final_data)
 
-def rule_human(row):
-    if row == 'human':
+
+def rule(x, row):
+    if x == row:
         return 1
     else:
         return 0
-def rule_robot(row):
-    if row == 'robot':
-        return 1
-    else:
-        return 0
+
+
 row_data = list(data['whoAmI'])
-convert_rows_human = list(rule_human(el) for el in row_data)
-convert_rows_robot = list(rule_robot(el) for el in row_data)
+row_1 = "human"
+convert_rows_human = list(rule(el, row_1) for el in row_data)
+row_2 = "robot"
+convert_rows_robot = list(rule(el, row_2) for el in row_data)
 
 res_data = pd.DataFrame({"human": convert_rows_human, "robot": convert_rows_robot})
 print("one hot вид")
